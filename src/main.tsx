13a983989fd5ec358d8ac657de1714fced279f4a
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-30 14:07:32 +0100

    update the app to use Auth0

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+    domain="dev-vkjc2qc55bwr1ikt.eu.auth0.com"
+    clientId="7I7v2p9IAvmwm0L3Aunc0NiMZkpAI1aQ"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+      audience: "https://tweetle/api"
+    }}
+    cacheLocation="localstorage"
+    useRefreshTokens={true}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </StrictMode>,
 )
